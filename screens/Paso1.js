433069cfaa57c790f0963b6903592e73eefe3613
Tu Nombre <pablitopacheco051@gmail.com>
2025-05-18 17:16:00 -0400

    antes de la actualizacion

---
+++ screens/Paso1.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { COLORS } from "../constants/colors";
+import { SIZES } from "../constants/sizes";
+import MyButton from "../components/MyButton";
+
